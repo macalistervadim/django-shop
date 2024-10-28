ae589dbb4deda6eeea5b5192dd51f87b949c1ae9
macalistervadim <macalistervadim@yandex.ru>
2024-10-28 18:15:13 +0600

    add new views in shop/views.py, add urls.py in shop/urls.py

---
+++ myshop/shop/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+import shop.views
+
+
+app_name = "shop"
+
+urlpatterns = [
+    path("", shop.views.product_list, name="product_list"),
+    path("<slug:category_slug>/", shop.views.product_list, name="product_list_by_category"),
+    path("<int:id>/<slug:slug>/", shop.views.product_detail, name="product_detail"),
+]
---
+++ myshop/shop/views.py
@@ -0,0 +1,26 @@
+import django.shortcuts
+
+import shop.models
+
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = shop.models.Category.objects.all()
+    products = shop.models.Product.objects.filter(available=True)
+
+    if category_slug:
+        category = django.shortcuts.get_object_or_404(
+            shop.models.Category, slug=category_slug,
+        )
+        products = products.filter(category=category)
+
+    return django.shortcuts.render(request, "shop/product/list.html", {"category": category, "categories": categories, "products": products})
+
+
+def product_detail(request, id, slug):
+    product = django.shortcuts.get_object_or_404(
+        shop.models.Product,
+        id=id, slug=slug, available=True,
+    )
+
+    return django.shortcuts.render(request, "shop/product/detail.html", {"product": product})
